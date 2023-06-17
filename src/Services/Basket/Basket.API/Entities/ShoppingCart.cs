44d64d359fdef222c38cf487eca290f4910857a4
diogofr <diogofr@live.com.pt>
2023-06-17 18:26:06 +0100

    add - BasketRepository

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,25 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShopingCartItem> Items { get; set; } = new List<ShopingCartItem>();
+
+        public ShoppingCart() { }
+
+        public ShoppingCart(string name) 
+        {
+            UserName = name;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                totalPrice = Items.Sum(x => x.Price * x.Quantity);
+                return totalPrice;
+            }
+        }
+    }
+}
