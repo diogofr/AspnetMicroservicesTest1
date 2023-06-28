1483f77277c0990ab5b2a568289f89485958d0e1
diogofr <diogofr@live.com.pt>
2023-06-28 19:33:36 +0100

    add - Discount controller

--- src/Services/Discount.API/Program.cs
+++ src/Services/Discount.API/Program.cs
@@ -1,3 +1,5 @@
+using Discount.API.Repositories;
+
 namespace Discount.API
 {
     public class Program
@@ -12,6 +14,9 @@ namespace Discount.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            var services = builder.Services;
+
+            services.AddScoped<IDiscountRepository, DiscountRepository>();
 
             var app = builder.Build();
 
