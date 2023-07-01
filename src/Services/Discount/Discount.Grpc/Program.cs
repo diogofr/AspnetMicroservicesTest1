80ae61e0e3a14ebed6294acfd561456d8926aacc
diogofr <diogofr@live.com.pt>
2023-07-01 16:20:01 +0100

    add - Discount.Common to Grpc

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,3 +1,5 @@
+using Discount.Common.Repositories;
+using Discount.Common.Extensions;
 using Discount.Services;
 
 namespace Discount.Grpc
@@ -12,13 +14,17 @@ namespace Discount.Grpc
             // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
             // Add services to the container.
-            builder.Services.AddGrpc();
+            var services = builder.Services;
+            services.AddGrpc();
+            services.AddScoped<IDiscountRepository, DiscountRepository>();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             app.MapGrpcService<GreeterService>();
             app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+            app.MigrateDatabase<Program>();
 
             app.Run();
         }
