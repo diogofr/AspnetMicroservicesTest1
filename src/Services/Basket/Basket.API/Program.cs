2ecc0b407dcbf128d7602088d81af37e082001a3
diogofr <diogofr@live.com.pt>
2023-07-01 20:41:20 +0100

    add - getDiscount to Basket.API

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,6 @@
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
+using Discount.Grpc.Protos;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +17,11 @@ var services = builder.Services;
 IConfiguration configuration = builder.Configuration;
 services.AddStackExchangeRedisCache(options => options.Configuration = configuration.GetValue<string>("CacheSettings:ConnectionString"));
 services.AddScoped<IBasketRepository, BasketRepository>();
+
+services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(o => o.Address = new Uri(configuration["GprcSettings:DiscountUrl"]));
+services.AddScoped<DiscountGrpcService>();
  
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
